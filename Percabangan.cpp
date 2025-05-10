132d4d7d25d5ff89f1fa617222bbb94ef26bfd0c
Azaria <rayandhika@students.amikom.ac.id>
2025-05-10 23:46:10 +0700

    biar cantik

--- Percabangan.cpp
+++ Percabangan.cpp
@@ -1,13 +1,28 @@
 #include <iostream>
+#include <string>
+
 using namespace std;
 
-int main (){
-	int jawaban;
-	
-	cout << "Berapakah hasil 3+5"<<endl;
-	cout << "jawab>";
-	cin >> jawaban;
-	
-	string hasil = (jawaban == 8)? "benar":"salah";
-	cout << "Jawaban anda:"<<hasil<<endl;
+int main() {
+    int jawaban;
+    int hasilSebenarnya = 3 + 5;
+
+    cout << "=======================================" << endl;
+    cout << "         PERTANYAAN MATEMATIKA         " << endl;
+    cout << "=======================================" << endl;
+    cout << "Berapakah hasil dari " << 3 << " + " << 5 << "? Silakan jawab: ";
+    cin >> jawaban;
+
+    string hasil;
+    if (jawaban == hasilSebenarnya) {
+        hasil = "? Selamat! Jawaban Anda benar. ?";
+    } else {
+        hasil = "? Maaf, jawaban Anda salah. Hasil yang benar adalah " << hasilSebenarnya << ". ?";
+    }
+
+    cout << "---------------------------------------" << endl;
+    cout << "Hasil Pemeriksaan: " << hasil << endl;
+    cout << "---------------------------------------" << endl;
+
+    return 0;
 }
